e48cba6fcc0d1cdbcb383f869a1aadf837fd015f
Elie Andraos <elie@andraos.org>
2018-08-14 11:34:10 +0300

    :lipstick: added active class for sidebar item

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,6 +1,10 @@
 <ul class='sidebar-menu'>
 	<li class='header'>Manage</li>
-	<li><a href='{!! route('areas.index') !!}'><i class="fa fa-cube" aria-hidden="true"></i> Areas</a></li>
+	<li>
+		<a href='{!! route('areas.index') !!}' @if(URL::current() == URL::to('areas')) class='active' @endif>
+			<i class="fa fa-cube" aria-hidden="true"></i> Areas
+		</a>
+	</li>
 	<li><a href='#'><i class="fa fa-home" aria-hidden="true"></i> Properties</a></li>
 
 </ul>
